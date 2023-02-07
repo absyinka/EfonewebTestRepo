f4b8770161d45862c9ad1fbf64aedd81d46da475
absyinka <absyinka@gmail.com>
2023-02-07 23:03:00 +0100

    Minor change

--- CSharpClass/Program.cs
+++ CSharpClass/Program.cs
@@ -7,11 +7,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        // AddSwapNumbers a = new AddSwapNumbers();
-        // a.AddSwapNos();            
-        // AddTwoNums atn = new AddTwoNums();
-        // atn.Add2Nums();
-        // atn.AddSNums();
         ClassOne classOne = new();
         var result = classOne.Add2();
         Console.WriteLine(result);
